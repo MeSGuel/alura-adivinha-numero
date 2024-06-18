33914c33332282bf8ecd689d1fd0d7e2c1457a65
MeSGuel <miguelmgsantos64@gmail.com>
2024-06-18 10:21:15 -0300

    Modificações de todos os arquivos e numero de limites adicionados

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
-let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+let numeroLimite = 10;
+// Limite de tentativas para acertar.
+let numeroDeTentativas = 10;
+let numeroSecreto = gerarNumeroAleatorio();
 
+let textoTentativas = document.getElementById('tentativas');
+textoTentativas.innerHTML = `
+    <p class="texto__pequeno" id="tentativas">
+    <i class="fa-solid fa-circle-info"></i>Você tem 10 tentativas</p>
+`;
+
+// Funções de Exibir texto
+exibirMensagemInicial();
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    // responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
@@ -14,16 +24,28 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
-exibirMensagemInicial();
 
+// Funções essenciais
 function verificarChute() {
     let chute = document.querySelector('input').value;
+
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+    let tentativasFalhas = `Você errou ${tentativas} vezes!`;
     
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        let palavraSobrou = numeroDeTentativas == 2 ? 'Sobrou' : 'Sobraram';
+        palavraTentativa = numeroDeTentativas == 2 ? 'tentativa' : 'tentativas';
+
+        numeroDeTentativas--;
+        textoTentativas.innerHTML = `
+            <p class="texto__pequeno" id="tentativas">
+            <i class="fa-solid fa-circle-info"></i>${palavraSobrou} ${numeroDeTentativas} ${palavraTentativa}</p>
+        `;
+
+        document.getElementById('chutar').setAttribute('disabled', true);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
@@ -31,8 +53,19 @@ function verificarChute() {
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
+
         tentativas++;
+        reduzNumeroDeTentativas();
         limparCampo();
+    }
+
+    // Condicional para ativar o "Game Over".
+    if(tentativas == 11) {
+        exibirTextoNaTela('h1', 'Você infelizmente fracassou!');
+        exibirTextoNaTela('p', tentativasFalhas);
+
+        document.getElementById('chutar').setAttribute('disabled', true);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }
 }
 
@@ -43,6 +76,7 @@ function gerarNumeroAleatorio() {
     if (quantidadeDeElementosNaLista == numeroLimite) {
         listaDeNumerosSorteados = [];
     }
+
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio();
     } else {
@@ -52,22 +86,43 @@ function gerarNumeroAleatorio() {
     }
 }
 
+function reduzNumeroDeTentativas() {
+    numeroDeTentativas--;
+    textoTentativas.innerHTML = `
+        <p class="texto__pequeno" id="tentativas">
+        <i class="fa-solid fa-circle-info"></i>Você tem ${numeroDeTentativas} tentativas</p>
+    `;
+
+    if(numeroDeTentativas == 1) {
+    textoTentativas.innerHTML = `
+        <p class="texto__pequeno" id="tentativas">
+        <i class="fa-solid fa-circle-info"></i>Você tem ${numeroDeTentativas} tentativa</p>
+    `;
+    } 
+    if(numeroDeTentativas == 0) {
+    textoTentativas.innerHTML = `
+        <p class="texto__pequeno" id="tentativas">
+        <i class="fa-solid fa-circle-info"></i>Você não tem mais nada...</p>
+    `;
+    }
+
+    console.log(numeroDeTentativas);
+}
+
 function limparCampo() {
-    chute = document.querySelector('input');
+    let chute = document.querySelector('input');
     chute.value = '';
 }
 
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
-    limparCampo();
     tentativas = 1;
+    numeroDeTentativas = 11;
+
+    reduzNumeroDeTentativas();
+    limparCampo();
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true)
+
+    document.getElementById('chutar').removeAttribute('disabled');
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
-
-
-
-
-
-
-
